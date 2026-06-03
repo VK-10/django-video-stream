bf239d1ed16d03503e9d67509d69764821e03639
light <visu2266@gmail.com>
2026-06-03 08:44:01 +0530

    update: consumer.py

--- video_streaming/watchparty/consumer.py
+++ video_streaming/watchparty/consumer.py
@@ -124,5 +124,79 @@ class WatchPartyConsumer(AsyncWebsocketConsumer):
             )
 
         async def addToPlaylistRecieve(text_data_json):
-            pass
+            response = requests.get('http://noembed.com/embed?rl=https://www.youtube.com/watch?v=' + data)
+            title = response.json()['title']
+            room.index += 1
+            room.add_to_playlist({"video_id": data, "title":title,  "index": room.index})
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    'type': "addToPlaylistSend",
+                    'action': action,
+                    'video_id': data,
+                    'title': title,
+                    'index': room.index,
+                    'username': self.user.username,
+                }
+            )
+        
+        async def removeFromPlaylistRecieve(text_data_json):
+            print("remove from playlist:" + data, data.split("*")[-1])
+            room.remove_from_playlist(data, data.split("*")[-1])
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    'type': 'removeFromPlaylistSend',
+                    'action': action,
+                    'data': data,
+                    'username': self.user.username, 
+                }
+            )
 
+        async def loadVideoRecieve(text_data_json):
+            action  = text_data_json['action']
+            data = text_data_json['data']
+            room.curr_video(data)
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    'type': 'loadVideoSend',
+                    'action': action,
+                    'data': data,
+                    'username': self.user.username,
+                }
+            )
+
+        async def playerStateChangeRecieve(text_data_json):
+            action = text_data_json['action']
+            room = rooms[self.room_group_name]
+            if action == "seek":
+                data = text_data_json['data']
+                await self.channel_layer.group_send(
+                    self.room_group_name,
+                    {
+                        'type': 'playerStateChangeSend',
+                        'action': action,
+                        'data': data,
+                        'username': self.user.username,
+                    }
+                )
+
+            else:
+                if action == "pause" or action == "play":
+                    room.SetPlayerState(action)
+                    await self.channel_layer.group_send(
+                        self.room_group_name,
+                        {
+                            'type': 'playerStateChangeSend',
+                            'action': action,
+                            'username': self.user.username,
+                        }
+                    )
+
+        recieved = {"username": setUserName, "message": messageRecieve, 
+                    "new_user_time": NewUserTimeRecieve, "addToPlaylist": addToPlaylistRecieve, 
+                    'removeFromPlaylist': removeFromPlaylistRecieve, 'loadVideo': loadVideoRecieve, 
+                    'play': playerStateChangeRecieve, 'pause': playerStateChangeRecieve, "seek": playerStateChangeRecieve}
+        
+        await recieved.get(text_data_json['info'])(text_data_json)
